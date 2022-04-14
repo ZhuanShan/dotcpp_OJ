d3050b1b6eae846f3e04d04794a9070a115216a8
zhuanshan <8434495+zhuanshan@user.noreply.gitee.com>
2022-04-14 16:59:38 +0800

    4.14

--- 1020.cpp
+++ 1020.cpp
@@ -2,3 +2,14 @@
 #include <iostream>
 using namespace std;
 
+int main(){
+    int n;
+    int sum=1;
+    cin>>n;
+    for(int i=1;i<n;i++){
+        sum = sum+1;
+        sum= sum*2;
+    }
+    cout<<sum;
+    return 0;
+}
